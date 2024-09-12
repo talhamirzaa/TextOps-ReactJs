b70a77ede044e210e57df37b0c4a7487d3d60992
talhamirzaa <mirzatalha0705@gmail.com>
2024-09-12 22:21:43 +0530

    search bar removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/">{props.name}</Link>
+    <Link className="navbar-brand" to="/">{props.name}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -23,10 +23,6 @@ export default function Navbar(props) {
             <NavLink className="nav-link" aria-current="page" to="/service" activeClassName="active">Service</NavLink>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
           <div className="form-check form-switch mx-5">
             <input className="form-check-input" type="checkbox" onClick={props.togglemode} role="switch" id="flexSwitchCheckDefault"/>
             <label className={`form-check-label text-${props.mode==='light'?'dark':'light'}`} htmlFor="flexSwitchCheckDefault">Enable {props.mode==='light'?'Dark':'Light'} Mode</label>
